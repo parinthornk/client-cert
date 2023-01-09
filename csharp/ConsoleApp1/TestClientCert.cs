8faa930773b48bfa497705bb7fbfedc0742e04a0
parinthornk <parinthorn.kett@outlook.com>
2023-01-09 23:25:26 +0700

    Added      New Text Document.txt Added      client.js Added      csharp/ConsoleApp1/App.config Added      csharp/ConsoleApp1/ConsoleApp1.csproj Added      csharp/ConsoleApp1/Program.cs Added      csharp/ConsoleApp1/Properties/AssemblyInfo.cs Added      csharp/ConsoleApp1/TestClientCert.cs Added      csharp/ConsoleApp1/packages.config Added      doesnt-work-certs/client-ca-crt.pem Added      doesnt-work-certs/client-ca-crt.srl Added      doesnt-work-certs/client-ca-key.pem Added      doesnt-work-certs/client-crt.pem Added      doesnt-work-certs/client-csr.pem Added      doesnt-work-certs/client-key.pem Added      doesnt-work-certs/server-ca-crt.pem Added      doesnt-work-certs/server-ca-crt.srl Added      doesnt-work-certs/server-ca-key.pem Added      doesnt-work-certs/server-crt.pem Added      doesnt-work-certs/server-csr.pem Added      doesnt-work-certs/server-key.pem Added      doesnt-work-client/.rnd Added      doesnt-work-client/client-ca-crt.pem Added      doesnt-work-client/client-ca-crt.srl Added      doesnt-work-client/client-ca-key.pem Added      doesnt-work-client/client-crt.pem Added      doesnt-work-client/client-csr.pem Added      doesnt-work-client/client-key.pem Added      doesnt-work-server/.rnd Added      doesnt-work-server/server-ca-crt.pem Added      doesnt-work-server/server-ca-crt.srl Added      doesnt-work-server/server-ca-key.pem Added      doesnt-work-server/server-crt.pem Added      doesnt-work-server/server-csr.pem Added      doesnt-work-server/server-key.pem Added      server.js Added      ssl-client/cert.txt Added      ssl-client/csr.txt Added      ssl-client/extracted-ph-cert.txt Added      ssl-client/extracted-ph-key.txt Added      ssl-client/key.txt Added      ssl-client/ph-cert.txt Added      ssl-client/ph-certandkey.pfx Added      ssl-client/ph-csr.txt Added      ssl-client/ph-key.txt Added      ssl-server/cert.txt Added      ssl-server/csr.txt Added      ssl-server/key.txt Added      ssl-server/ph-cert.txt Added      ssl-server/ph-csr.txt Added      ssl-server/ph-key.txt

---
+++ csharp/ConsoleApp1/TestClientCert.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1
+{
+    internal class TestClientCert
+    {
+        public TestClientCert()
+        {
+            ServicePointManager.ServerCertificateValidationCallback = (_1, _2, _3, _4) =>
+            {
+                return true;
+            };
+
+
+            var handler = new HttpClientHandler();
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            handler.SslProtocols = SslProtocols.Tls12;
+            handler.ClientCertificates.Add(new X509Certificate2(@"C:\Users\Admin\Desktop\mutual_authentication_example\ssl-client\ph-certandkey.pfx", "client"));
+
+            var client = new HttpClient(handler);
+            client.BaseAddress = new Uri("https://localhost:8888");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = client.GetAsync("/").Result;
+
+            var statusCode = response.StatusCode;
+            var contents = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine("statusCode: " + statusCode);
+            Console.WriteLine("contents: " + contents);
+        }
+    }
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const https = require("https");
+const options = {
+	key: fs.readFileSync(`${__dirname}/ssl-server/ph-key.txt`),
+	passphrase: "localhost", // ----> optional
+	cert: fs.readFileSync(`${__dirname}/ssl-server/ph-cert.txt`),
+	ca: [
+		fs.readFileSync(`${__dirname}/ssl-client/ph-cert.txt`)
+	],
+	requestCert: true,
+	rejectUnauthorized: true
+};
+https.createServer(options, function(req, res) {
+	console.log(new Date() + " " + req.connection.remoteAddress + " " + req.method + " " + req.url);
+    res.writeHead(200);
+    res.end("OK!\n");
+}).listen(8888);
